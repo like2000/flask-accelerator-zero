d05a181d75b358d816c49efab432f9268a9f3f27
Kevin Li <kli@cern.ch>
2020-05-10 16:10:52 +0200

    Add compiled angular app.

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,19 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='backend/static/')
 api = Api(app)
 
 
 @app.route('/<path:path>', methods=['GET'])
 def static_proxy(path):
-    return send_from_directory('./', path)
+    return send_from_directory('backend/static', path)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return send_from_directory('backend/static', 'index.html')
 
 
 if __name__ == '__main__':
