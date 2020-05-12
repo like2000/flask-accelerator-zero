54b8df3f2ee816b8824f0dafdfaf2f6fc2d32a82
Kevin Li <kli@cern.ch>
2020-05-12 20:10:56 +0200

    Simplify CORS.

--- wsgi.py
+++ wsgi.py
@@ -5,33 +5,34 @@ from backend.entities import Entity
 
 app = Flask(__name__,
             static_folder='backend/static/')
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-# CORS(app)
-cors = CORS(app, resources={r"*": {"origins": "*"}})
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 api = Api(app)
 api.add_resource(Entity, '/data')
 
+CORS(app)
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
+
+# cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+#     return response
 
 
 @app.route('/<path:path>', methods=['GET', 'POST'])
 # @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-@cross_origin(origin='*')
+@cross_origin()
 def static_proxy(path):
     return send_from_directory('backend/static', path)
 
 
 @app.route('/', methods=['GET', 'POST'])
 # @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-@cross_origin(origin='*')
+@cross_origin()
 def hello_world():
     return send_from_directory('backend/static', 'index.html')
 
