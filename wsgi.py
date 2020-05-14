a552fe4cb77a6cf028e1f728e242407a37604fc6
Kevin Li <kli@cern.ch>
2020-05-14 23:48:11 +0200

    Clean up main

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,9 @@
-from flask import Flask, send_from_directory, render_template, request, redirect, url_for
+from flask import send_from_directory
 from flask_cors import CORS, cross_origin
-from flask_restful import Api, Resource
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# from backend.book import Book, Base
 
 from backend import create_app
-from backend.entities import Entity
+
+# from backend.book import Book, Base
 
 # api = Api(app)
 # api.add_resource(Entity, '/data')
@@ -25,7 +21,24 @@ from backend.entities import Entity
 #
 #     return {'data': [df.to_dict(orient='records')]}
 
-# print("Running from inside main!")
+# app = create_app()
+# CORS(app)
+#
+#
+# @app.route('/<path:path>', methods=['GET', 'POST'])
+# @cross_origin()
+# def static_proxy(path):
+#     return send_from_directory('static', path)
+#
+#
+# @app.route('/', methods=['GET', 'POST'])
+# @cross_origin()
+# def index():
+#     return send_from_directory('static', 'index.html')
+
+
+# if __name__ == '__main__':
+print("Running from inside main!")
 app = create_app()
 CORS(app)
 
@@ -45,15 +58,6 @@ def index():
 # try:
 host = "https://accelerator-zero.herokuapp.com"
 app.run(host=host)
-
-if __name__ == '__main__':
-    print("Running from inside main!")
-    app = create_app()
-    CORS(app)
-
-    # try:
-    host = "https://accelerator-zero.herokuapp.com"
-    app.run(host=host)
-    # except:
-    #     host="localhost"
-    #     app.run(host=host)
+# except:
+#     host="localhost"
+#     app.run(host=host)
